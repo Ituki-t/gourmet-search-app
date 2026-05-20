abf233ac710d847c5c9b79a88ff8704de018b05c
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-05-20 17:50:43 +0900

    hotpepper.pyでデータを取得できた

---
+++ restaurants/hotpepper.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from dotenv import load_dotenv
+from pprint import pprint
+
+load_dotenv()
+api_key = os.getenv('HOTPEPPER_API_KEY')
+api_url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+
+def restaurants_list():
+    params = {
+        'key': api_key,
+        'format': 'json',
+        'count': 10,
+        'keyword': '東京',
+    }
+    res = requests.get(api_url, params=params)
+    datas = res.json()
+    # print(datas)
+
+    restaurants = []
+    for data in datas['results']['shop']:
+        restaurant = {
+            'name': data['name'],
+            'address': data['address'],
+            'genre': data['genre']['name'],
+            'budget': data['budget']['name'],
+            'photo': data['photo']['pc']['l'],
+        }
+        restaurants.append(restaurant)
+    return restaurants
+pprint(restaurants_list())
