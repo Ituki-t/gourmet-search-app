94d7c1bce6a566ea0b4f0330107266a4a4b2a259
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-05-24 11:56:14 +0900

    緯度, 経度の情報をsession strageに保存

--- restaurants/static/js/geolocation.js
+++ restaurants/static/js/geolocation.js
@@ -7,13 +7,26 @@ function successCallback(position){
     console.log('緯度: ' + position.coords.latitude, '経度: ' + position.coords.longitude);
 
     // alert('緯度: ' + position.coords.latitude + '\n経度: ' + position.coords.longitude);
-    var latitude = position.coords.latitude;
+    const latitude = position.coords.latitude;
     document.getElementById('latitude').value = latitude;
+    sessionStorage.setItem('lat', latitude);
 
-    var longitude = position.coords.longitude;
+    const longitude = position.coords.longitude;
     document.getElementById('longitude').value = longitude;
+    sessionStorage.setItem('lng', longitude);
 }
 
 function errorCallback(error){
     console.error('位置情報の取得に失敗しました。');
 }
+
+
+window.addEventListener('load', function() {
+    const lat = sessionStorage.getItem('lat');
+    const lng = sessionStorage.getItem('lng');
+
+    if (lat && lng) {
+        document.getElementById('latitude').value = lat;
+        document.getElementById('longitude').value = lng;
+    }
+})
