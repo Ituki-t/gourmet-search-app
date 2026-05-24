fde55a7ede8af1064b93013797450d1f19a73d1f
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-05-24 14:33:39 +0900

    型のテスト

--- restaurants/views.py
+++ restaurants/views.py
@@ -11,23 +11,29 @@ from pprint import pprint
 def index(request):
     lat = request.GET.get('lat')
     lng = request.GET.get('lng')
-    range = request.GET.get('range')
+    range_value = request.GET.get('range')
     # keyword = request.GET.get('keyword')
 
-    # ページング対応
-    page = request.GET.get('page', 1)
-    count = request.GET.get('count', 10) # 1ページあたりの件数
-    start = (int(page) - 1) * count + 1
-    prev_page = int(page) - 1 if int(page) > 1 else None
-    next_page = int(page) + 1
+    # ページング
+    page = int(request.GET.get('page', 1))
+    count = int(request.GET.get('count', 10)) # 1ページあたりの件数
+    start = (page - 1) * count + 1
 
-    restaurants, results_available = restaurants_list(lat=lat, lng=lng, range=range, start=start)
-    pprint(restaurants)
+    restaurants, results_available = restaurants_list(lat=lat, lng=lng, range=range_value, start=start)
+
+    # pprint(restaurants)
+    print(type(page))
+    print(type(results_available))
+    print(type(count))
+
+    prev_page = page - 1 if page > 1 else None
+    next_page = page + 1 if results_available > page * count else None
+
     context = {
         'restaurants': restaurants,
         'lat': lat,
         'lng': lng,
-        'range': range,
+        'range': range_value,
         'results_available': results_available,
         'page': page,
         'prev_page': prev_page,
